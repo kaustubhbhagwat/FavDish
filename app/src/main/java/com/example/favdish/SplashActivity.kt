3d435b24b23bcb6b7b62825defba125687dc0efd
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-04 19:17:26 +0530

    Splash Screen

---
+++ app/src/main/java/com/example/favdish/SplashActivity.kt
@@ -0,0 +1,17 @@
+package com.example.favdish
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.favdish.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+
+
+
+    }
+}
