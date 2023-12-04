0b2b3c0143cd94249d024a006f5ba91f9ad4fd57
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-04 19:56:48 +0530

    Splash Screen

--- app/src/main/java/com/example/favdish/SplashActivity.kt
+++ app/src/main/java/com/example/favdish/SplashActivity.kt
@@ -1,7 +1,16 @@
 package com.example.favdish
 
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.example.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -10,8 +19,33 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
 
+        val splashAnimation = AnimationUtils.loadAnimation(this, R.anim.anim_splash)
+        splashBinding.tvAppName.animation = splashAnimation
+        splashAnimation.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation?) {
+                //
+            }
 
+            override fun onAnimationEnd(animation: Animation?) {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                },500)
+            }
+
+            override fun onAnimationRepeat(animation: Animation?) {
+                finish()
+            }
+        } )
 
     }
 }
