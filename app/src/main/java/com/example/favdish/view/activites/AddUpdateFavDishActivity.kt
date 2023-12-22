94c25b56306f650cea6cf01b5a621e06a9fab848
Kaustubh <kaustubhbhagwat93@gmail.com>
2023-12-22 19:33:43 +0530

    added menu for add

---
+++ app/src/main/java/com/example/favdish/view/activites/AddUpdateFavDishActivity.kt
@@ -0,0 +1,13 @@
+package com.example.favdish.view.activites
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.favdish.R
+
+class AddUpdateFavDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_fav_dish)
+
+    }
+}
---
+++ app/src/main/java/com/example/favdish/view/fragments/AllDishesFragment.kt
@@ -0,0 +1,70 @@
+package com.example.favdish.view.fragments
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.favdish.R
+import com.example.favdish.databinding.FragmentAllDishesBinding
+import com.example.favdish.view.activites.AddUpdateFavDishActivity
+import com.example.favdish.viewmodel.HomeViewModel
+
+class AllDishesFragment : Fragment() {
+
+    private var _binding: FragmentAllDishesBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+    private lateinit var homeViewModel: HomeViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentAllDishesBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_all_dishes,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       when(item.itemId){
+           R.id.action_add_dish ->
+               startActivity(Intent(requireActivity(),AddUpdateFavDishActivity::class.java))
+       }
+
+        return super.onOptionsItemSelected(item)
+
+
+    }
+}
