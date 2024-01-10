3c8d017e01f1668139eb127fd264eb96ccb93053
Kaustubh <kaustubhbhagwat93@gmail.com>
2024-01-10 19:37:41 +0530

    android camera image click

--- app/src/main/java/com/example/favdish/view/activites/AddUpdateFavDishActivity.kt
+++ app/src/main/java/com/example/favdish/view/activites/AddUpdateFavDishActivity.kt
@@ -1,12 +1,16 @@
 package com.example.favdish.view.activites
 
+import android.Manifest.permission.CAMERA
+import android.app.Activity
 import android.app.AlertDialog
 import android.app.Dialog
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.View
 import android.view.View.OnClickListener
@@ -17,8 +21,11 @@ import com.example.favdish.databinding.DialogCustomImageSelectionBinding
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
+import com.karumi.dexter.listener.single.PermissionListener
 
 class AddUpdateFavDishActivity : AppCompatActivity(), OnClickListener {
     private lateinit var binding: ActivityAddUpdateFavDishBinding
@@ -55,21 +62,31 @@ class AddUpdateFavDishActivity : AppCompatActivity(), OnClickListener {
             DialogCustomImageSelectionBinding.inflate(layoutInflater)
         dialog.setContentView(binding.root)
 
-        binding.tvCamera.setOnClickListener {
-            Dexter.withContext(this).withPermissions(
-            android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                android.Manifest.permission.CAMERA
-            ).withListener(object : MultiplePermissionsListener{
-                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if(report!!.areAllPermissionsGranted()){
-                        Toast.makeText(this@AddUpdateFavDishActivity,"All permissions granted",Toast.LENGTH_LONG).show()
-                    }
+        // GALLERY
+        binding.tvGallery.setOnClickListener {
+            Dexter.withContext(this).withPermission(
+                android.Manifest.permission.READ_EXTERNAL_STORAGE,
+            ).withListener(object : PermissionListener {
+
+                override fun onPermissionGranted(p0: PermissionGrantedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateFavDishActivity,
+                        "You have the gallery permission to select an image",
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+
+                override fun onPermissionDenied(p0: PermissionDeniedResponse?) {
+                    Toast.makeText(
+                        this@AddUpdateFavDishActivity,
+                        "You don't have the gallery permission to select an image",
+                        Toast.LENGTH_LONG
+                    ).show()
                 }
 
                 override fun onPermissionRationaleShouldBeShown(
-                    permissions: MutableList<PermissionRequest>?,
-                    token: PermissionToken?
+                    p0: PermissionRequest?,
+                    p1: PermissionToken?
                 ) {
                     showRationalDialigforPermissions()
                 }
@@ -78,15 +95,17 @@ class AddUpdateFavDishActivity : AppCompatActivity(), OnClickListener {
             dialog.dismiss()
         }
 
-        binding.tvGallery.setOnClickListener {
+        // CAMERA
+        binding.tvCamera.setOnClickListener {
             Dexter.withContext(this).withPermissions(
-                android.Manifest.permission.READ_EXTERNAL_STORAGE,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 android.Manifest.permission.CAMERA
-            ).withListener(object : MultiplePermissionsListener{
+            ).withListener(object : MultiplePermissionsListener {
                 override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
-                    if(report!!.areAllPermissionsGranted()){
-                        Toast.makeText(this@AddUpdateFavDishActivity,"All have Gallery permissions!! All good to go",Toast.LENGTH_LONG).show()
+                    report?.let {
+                        if (report.areAllPermissionsGranted()) {
+                            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                            startActivityForResult(intent, CAMERA)
+                        }
                     }
                 }
 
@@ -99,29 +118,49 @@ class AddUpdateFavDishActivity : AppCompatActivity(), OnClickListener {
 
             }).onSameThread().check()
             dialog.dismiss()
-
 
 
         }
         dialog.show()
     }
 
-    private fun showRationalDialigforPermissions(){
-        AlertDialog.Builder(this).setMessage("It looks like you have turned off permissions required for this feature")
+    private fun showRationalDialigforPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It looks like you have turned off permissions required for this feature")
             .setPositiveButton("Go to settings")
-            {_,_ ->
+            { _, _ ->
                 try {
                     val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                    val uri = Uri.fromParts("package",packageName, null)
+                    val uri = Uri.fromParts("package", packageName, null)
                     intent.data = uri
                     startActivity(intent)
-                }catch (e : ActivityNotFoundException){
+                } catch (e: ActivityNotFoundException) {
                     e.printStackTrace()
                 }
             }
-            .setNegativeButton("Cancel"){dialog,_ ->
+            .setNegativeButton("Cancel") { dialog, _ ->
                 dialog.dismiss()
             }.show()
 
     }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.let {
+                    val thumbnail: Bitmap = data.extras!!.get("data") as Bitmap
+                    binding.ivDishImage.setImageBitmap(thumbnail)
+                }
+            }
+        }
+    }
+
+    companion object {
+        const val CAMERA = 1
+        const val GALLERY = 2
+    }
 }
+
+
