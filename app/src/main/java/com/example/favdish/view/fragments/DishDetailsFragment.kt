c08b668872a373eaa343486d3c3c2c8aa4f37d4a
kaustubhbhagwat <kaustubhbhagwat93@gmail.com>
2024-01-17 20:05:09 +0530

    jetpack palette

--- app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
+++ app/src/main/java/com/example/favdish/view/fragments/DishDetailsFragment.kt
@@ -1,18 +1,26 @@
 package com.example.favdish.view.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.fragment.navArgs
+import androidx.palette.graphics.Palette
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.example.favdish.databinding.FragmentDishDetailsBinding
 
 
 class DishDetailsFragment : Fragment() {
 
-    lateinit var binding : FragmentDishDetailsBinding
+    lateinit var binding: FragmentDishDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -30,7 +38,37 @@ class DishDetailsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val args: DishDetailsFragmentArgs by navArgs()
         args.let {
-            Glide.with(this@DishDetailsFragment).load(it.StringDishDetails.image).into(binding.dishDetailsImageView)
+            Glide.with(this@DishDetailsFragment).load(it.StringDishDetails.image)
+                .listener(object: RequestListener<Drawable>{
+                    override fun onLoadFailed(
+                        e: GlideException?,
+                        model: Any?,
+                        target: Target<Drawable>,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        Log.e("TAG","Error loading image")
+                        return false
+                    }
+
+                    override fun onResourceReady(
+                        resource: Drawable,
+                        model: Any,
+                        target: Target<Drawable>?,
+                        dataSource: DataSource,
+                        isFirstResource: Boolean
+                    ): Boolean {
+                        resource.toBitmap().let { it1 ->
+                            Palette.from(it1).generate{ palette->
+                                val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                                binding.dishDetailsParentLayout.setBackgroundColor(intColor)
+                            }
+                        }
+                        return false
+                    }
+
+                })
+                .into(binding.dishDetailsImageView)
+
             binding.dishTitle.text = it.StringDishDetails.title
             binding.dishType.text = it.StringDishDetails.type
             binding.dishIngredients.text = it.StringDishDetails.ingredients
